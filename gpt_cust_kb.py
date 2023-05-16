17546ebcfe75baf3b107d32239b4b873ffd224b7
jlonge4 <josh.longenecker1@gmail.com>
2023-05-15 20:23:35 -0400

    updated index saving/loading

--- gpt_cust_kb.py
+++ gpt_cust_kb.py
@@ -1,4 +1,4 @@
-from llama_index import download_loader, SimpleDirectoryReader, ServiceContext, GPTSimpleVectorIndex, LLMPredictor, GPTSimpleVectorIndex, PromptHelper
+from llama_index import download_loader, SimpleDirectoryReader, ServiceContext, LLMPredictor, GPTVectorStoreIndex, PromptHelper, StorageContext, load_index_from_storage
 from pathlib import Path
 import openai
 import os
@@ -9,24 +9,34 @@ import time
 from langchain.chat_models import ChatOpenAI
 
 
-os.environ['OPENAI_API_KEY'] = 'super secret key'
-
+key = os.getenv("OPENAI_API_KEY")
+os.environ['OPENAI_API_KEY'] = key
 llm_predictor = LLMPredictor(llm=ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo"))
 service_context = ServiceContext.from_defaults(llm_predictor=llm_predictor)
 
 #build index from PDF
-def pdf_to_index(pdf_path):
+def pdf_to_index(pdf_path, save_path):
     PDFReader = download_loader('PDFReader')
     loader = PDFReader()
     documents = loader.load_data(file=Path(pdf_path))
-    index = GPTSimpleVectorIndex.from_documents(documents)
-    index.save_to_disk('./test_chat.json')
+    index = GPTVectorStoreIndex.from_documents(documents)
+    # index.save_to_disk(save_path)
+    index.storage_context.persist(persist_dir=save_path)
+    print('saved to disk')
 
 
 #query index using GPT
 def query_index(index_path, query_u):
-    index = GPTSimpleVectorIndex.load_from_disk(index_path, service_context=service_context)
-    response=index.query(query_u)
+    # index = GPTVectorStoreIndex.load_from_disk(index_path, service_context=service_context)
+    PATH = 'C/gpt_indexes'
+    # rebuild storage context
+    storage_context = StorageContext.from_defaults(persist_dir=f"{PATH}/test.json")
+
+    # load index
+    index = load_index_from_storage(storage_context, service_context=service_context)
+    query_engine = index.as_query_engine()
+    response = query_engine.query(query_u)
+    # response=index.query(query_u)
     st.session_state.past.append(query_u)
     st.session_state.generated.append(response.response)   
 
@@ -35,18 +45,29 @@ def clear_convo():
     st.session_state['past'] = []
     st.session_state['generated'] = []
 
+def save_pdf(file):
+    PATH = 'C/gpt_indexes'
+    if not os.path.exists(PATH):
+        os.makedirs(PATH)
+        pdf_to_index(pdf_path='C:/'+file, save_path=f'{PATH}/{file}')
+        print('saving index')
+
 
 def get_manual():
+    manual_names = []
     manual = st.sidebar. radio("Choose a manual:", ("Yukon Test PDF", "LLM Research"))
     if manual== "Yukon Test":
-        return "./test.json"
+        return f"./{name_of_pdf}.json"
+
+def init():
+    st.set_page_config(page_title='PDF ChatBot', page_icon=':robot_face: ') 
+    st.sidebar.title('Available manuals')
+
 
 
 if __name__ == '__main__':
-    st.set_page_config(page_title='PDF ChatBot', page_icon=':robot_face: ') 
-    st.sidebar.title('Available manuals')
+    
     clear_button = st.sidebar.button("Clear Conversation", key="clear") 
-
     if clear_button:
         clear_convo()
 
@@ -71,4 +92,7 @@ if __name__ == '__main__':
             message(st.session_state['generated'][i], key=str(i))
             message(st.session_state['past'][i], is_user=True, key=str(i) + "user")
     
-    st.sidebar. radio("Choose a manual:", ("Yukon Test PDF", "LLM Research"))
+    st.sidebar.radio("Choose a manual:", ("Yukon Test PDF", "LLM Research"))
+    file = st.file_uploader("Choose a PDF file to index...")
+    save_pdf(file.name)
+    
